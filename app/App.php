e04d145cf882a45d490593e224197f1ce7c302cd
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-08 22:50:52 +0100

    created getTransactionFiles function

--- app/App.php
+++ app/App.php
@@ -3,3 +3,14 @@
 declare(strict_types = 1);
 
 // Your Code
+function getTransactionFiles(string $dirPath): array 
+{
+    $files = [];
+    foreach (scandir($dirPath) as $file) {
+        if(is_dir($file)){
+            continue;
+        }
+        $files[] = $file;
+    }
+    return $files;
+}
